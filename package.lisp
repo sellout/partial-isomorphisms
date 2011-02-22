0e13c6342cedda68a90c2f360520d8b4c1103acf
Greg Pfeil <greg@technomadic.org>
2011-02-22 09:39:46 -0500

    Export ISOMORPHISM.

--- package.lisp
+++ package.lisp
@@ -4,7 +4,7 @@
                           #:standard-class
                           #:standard-generic-function #:defgeneric
                           #:standard-method #:defmethod)
-  (:export #:make-isomorphism #:make-automorphism
+  (:export #:isomorphism #:make-isomorphism #:make-automorphism
            #:inverse #:apply #:funcall #:unapply #:unfuncall
            #:compose #:iso-identity
            #:tuple #:associate #:commute #:unit #:element #:subset))
